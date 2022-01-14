f4c139e1ead23d4417891c237da8e6c62cbb7cd5
Stuart Rench <sturench@gmail.com>
2022-01-14 16:50:39 -0600

    Initial commit

---
+++ CroMoonStats/__init__.py
@@ -0,0 +1,64 @@
+from datetime import datetime, timedelta
+from random import randint
+
+import requests
+
+
+class CroMoonStats:
+    def __init__(self):
+        self.total_supply = 1000000000000000
+        # self.last_dextools_update_time = datetime.fromtimestamp(0)
+        # self.dextools_result = None
+        # self.dextools_interval = timedelta(seconds=15)
+        self.last_cronoscan_update_time = datetime.fromtimestamp(0)
+        self.cronoscan_interval = timedelta(minutes=5)
+        self.cronoscan_result = None
+        self.last_crodex_update = datetime.fromtimestamp(0)
+        self.crodex_result = None
+        self.crodex_interval = timedelta(minutes=5)
+        self.cromoon_price = 0
+
+    def get_dead_wallet_string(self):
+        return f'{self.get_dead_wallet():,}'
+
+    def get_dead_wallet(self):
+        self.get_cronoscan()
+        count = self.cronoscan_result['result']
+        r = int(count) / 1000000000
+        return r
+
+    def get_current_price_string(self):
+        self.get_crodex()
+        return self.cromoon_price
+
+    def get_current_price(self):
+        self.get_crodex()
+        return float(self.cromoon_price)
+
+    def get_market_cap(self):
+        return self.get_current_price() * (self.total_supply - self.get_dead_wallet())
+
+    def get_percent_burned(self):
+        return self.get_dead_wallet()/self.total_supply
+
+    def get_cronoscan(self):
+        now = datetime.now()
+        if now > self.last_cronoscan_update_time + self.cronoscan_interval:
+            self.cronoscan_result = requests.get(
+                'https://api.cronoscan.com/api?module=account&action=tokenbalance&contractaddress=0x7d30c36f845d1dee79f852abf3a8a402fadf3b53&address=0x000000000000000000000000000000000000dEaD&tag=latest&apikey={}'.format(randint(10000,999999999))).json()
+            self.last_cronoscan_update_timescan_update_time = now
+
+    # def get_dextools(self):
+    #     now = datetime.now()
+    #     if now > self.last_dextools_update_time + self.dextools_interval:
+    #         self.dextools_result = requests.get(
+    #             'https://www.dextools.io/chain-cronos/api/pair/summary?address=0xb2ba36ee6ba6113a914f3e8812a0df094dec5994&foo=1234').json()
+    #         self.last_dextools_update_time = now
+
+    def get_crodex(self):
+        now = datetime.now()
+        if now > self.last_crodex_update + self.crodex_interval:
+            self.crodex_result = requests.get(
+                'https://chartapi.crodex.app/token/0x7D30c36f845d1dEe79f852abF3A8A402fAdF3b53/info?foo={}'.format(randint(10000,999999999))).json()
+            self.cromoon_price = self.crodex_result.get('priceInUSDT', "ERROR")
+            self.last_crodex_update = now
---
+++ main.py
@@ -0,0 +1,87 @@
+"""
+Simple Bot to reply to Telegram messages.
+
+First, a few handler functions are defined. Then, those functions are passed to
+the Dispatcher and registered at their respective places.
+Then, the bot is started and runs until we press Ctrl-C on the command line.
+
+Usage:
+Basic Echobot example, repeats messages.
+Press Ctrl-C on the command line or send a signal to the process to stop the
+bot.
+"""
+
+import logging
+
+import telegram
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from CroMoonStats import CroMoonStats
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+cromoon = CroMoonStats()
+
+
+# Define a few command handlers. These usually take the two arguments update and
+# context. Error handlers also receive the raised TelegramError object in error.
+def help(update, context):
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('At this point, you can just type /stats')
+
+
+def echo(update, context):
+    """Echo the user message."""
+    update.message.reply_text(update.message.text)
+
+
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+
+def get_stats(update, context):
+    reply_lines = [
+        '<b><u>CroMoon Statistics</u></b> <i>(updated every 5 minutes)</i>\n',
+        'Current price:                 ${}'.format(f'{cromoon.get_current_price():.12f}'),
+        'Current MC:                    ${}'.format(f'{cromoon.get_market_cap():,.2f}'),
+        'Burn wallet (0x0dead):   {} ({})'.format(f'{cromoon.get_percent_burned():.2%}', cromoon.get_dead_wallet_string()),
+    ]
+    update.message.reply_text('\n'.join(reply_lines), parse_mode=telegram.ParseMode.HTML)
+
+
+def main():
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
+    updater = Updater("5087275185:AAHk0apGHrgGBB7nSFl23pRThVoZXKL9Dwg", use_context=True)
+    global cromoon
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    # dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("stats", get_stats))
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    cromoon = CroMoonStats()
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
