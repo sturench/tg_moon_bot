efc4bc08c340568900ed31caad2a54c56c1f8553
Stuart Rench <sturench@gmail.com>
2022-01-14 17:27:21 -0600

    Switched to Dexscreener

--- CroMoonStats/__init__.py
+++ CroMoonStats/__init__.py
@@ -17,6 +17,9 @@ class CroMoonStats:
         self.crodex_result = None
         self.crodex_interval = timedelta(minutes=5)
         self.cromoon_price = 0
+        self.dexscreener_result = None
+        self.dexscreener_update_time = datetime.fromtimestamp(0)
+        self.dexscreener_interval = timedelta(minutes=5)
 
     def get_dead_wallet_string(self):
         return f'{self.get_dead_wallet():,}'
@@ -28,11 +31,11 @@ class CroMoonStats:
         return r
 
     def get_current_price_string(self):
-        self.get_crodex()
+        self.get_dexscreener()
         return self.cromoon_price
 
     def get_current_price(self):
-        self.get_crodex()
+        self.get_dexscreener()
         return float(self.cromoon_price)
 
     def get_market_cap(self):
@@ -55,10 +58,18 @@ class CroMoonStats:
     #             'https://www.dextools.io/chain-cronos/api/pair/summary?address=0xb2ba36ee6ba6113a914f3e8812a0df094dec5994&foo=1234').json()
     #         self.last_dextools_update_time = now
 
-    def get_crodex(self):
+    # def get_crodex(self):
+    #     now = datetime.now()
+    #     if now > self.last_crodex_update + self.crodex_interval:
+    #         self.crodex_result = requests.get(
+    #             'https://chartapi.crodex.app/token/0x7D30c36f845d1dEe79f852abF3A8A402fAdF3b53/info?foo={}'.format(randint(10000,999999999))).json()
+    #         self.cromoon_price = self.crodex_result.get('priceInUSDT', "ERROR")
+    #         self.last_crodex_update = now
+
+    def get_dexscreener(self):
         now = datetime.now()
-        if now > self.last_crodex_update + self.crodex_interval:
-            self.crodex_result = requests.get(
-                'https://chartapi.crodex.app/token/0x7D30c36f845d1dEe79f852abF3A8A402fAdF3b53/info?foo={}'.format(randint(10000,999999999))).json()
-            self.cromoon_price = self.crodex_result.get('priceInUSDT', "ERROR")
-            self.last_crodex_update = now
+        if now > self.dexscreener_update_time + self.dexscreener_interval:
+            self.dexscreener_result = requests.get(
+                'https://io9.dexscreener.io/u/trading-history/recent/cronos/0xb2bA36ee6ba6113a914f3E8812A0dF094DEC5994').json()
+            self.cromoon_price = self.dexscreener_result.get('tradingHistory', [])[0].get('priceUsd', '0')
+            self.dexscreener_update_time = now
